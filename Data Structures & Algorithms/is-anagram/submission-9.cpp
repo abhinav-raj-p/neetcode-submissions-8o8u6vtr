c2e50e6110dc7c6e33010e3bf3b640c521b13304
abhinav-raj-p <abhinavrajp05@gmail.com>
2026-07-20 07:18:31 +0530

    Add: is-anagram - submission-9

---
+++ Data Structures & Algorithms/is-anagram/submission-9.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int freq[26]={0};
+        if(s.length()!=t.length()){
+            return false;
+        }
+        for(char c:s){
+            freq[c-'a']++;
+        }
+        for(char c:t){
+            if(freq[c-'a']==0){
+                return false;
+            }
+            freq[c-'a']--;
+        }
+        return true;
+    }
+
+};
